272babf532ed96524887af3deecaf30e8e451d2a
shravanrn@gmail.com <shravanrn@gmail.com>
2019-05-06 00:13:59 -0700

    Web crawler code

--- webreslist.js
+++ webreslist.js
@@ -1,3 +1,183 @@
-(function(){
-    alert("Hello");
-})();
+//////////////////////////////////////////////////////////////////////////////////
+//Settings
+const delayAmount = 5000;
+const sites = ["https://developer.mozilla.org", "https://twitter.com/sggrc"];
+
+//////////////////////////////////////////////////////////////////////////////////
+
+var inProgress = false;
+var currentTab = -2;
+var allResults = {};
+const emptyResultRow = {
+    "count" : 0,
+    "originCount" : 0,
+    "gzippedCount" : 0,
+    "otherCompressedCount" : 0,
+    "origins" : []
+};
+const emptyResult = {
+    "alltext" : JSON.parse(JSON.stringify(emptyResultRow)),
+    "jpeg"    : JSON.parse(JSON.stringify(emptyResultRow)),
+    "png"     : JSON.parse(JSON.stringify(emptyResultRow)),
+    "html"    : JSON.parse(JSON.stringify(emptyResultRow)),
+    "css"     : JSON.parse(JSON.stringify(emptyResultRow)),
+    "js"      : JSON.parse(JSON.stringify(emptyResultRow))
+};
+var currentResult = JSON.parse(JSON.stringify(emptyResult));
+
+function updateResult(key, url, contentEncoding) {
+    currentResult[key].count++;
+
+    const origin = getOrigin(url);
+    if (!currentResult[key].origins.includes(origin)){
+        currentResult[key].origins.push(origin);
+        currentResult[key].originCount++;
+    }
+
+    if (isGZipped(contentEncoding)) {
+        currentResult[key].gzippedCount++;
+    } else if (isCompressed(contentEncoding)) {
+        currentResult[key].otherCompressedCount++;
+    }
+}
+
+function logURL(details) {
+    if (details.tabId != currentTab) {
+        return;
+    }
+
+    var contentType = last(details.responseHeaders.filter(contentHeader => contentHeader.name == "content-type"));
+    var contentEncoding = last(details.responseHeaders.filter(contentHeader => contentHeader.name == "content-encoding"));
+
+    if (isHTML(details.url, contentType)) {
+        updateResult("html", details.url, contentEncoding);
+    } else if (isCSS(details.url, contentType)) {
+        updateResult("css", details.url, contentEncoding);
+    } else if (isJS(details.url, contentType)) {
+        updateResult("js", details.url, contentEncoding);
+    } else if (isJPEG(details.url, contentType)) {
+        updateResult("jpeg", details.url, contentEncoding);
+    } else if (isPNG(details.url, contentType)) {
+        updateResult("png", details.url, contentEncoding);
+    }
+
+    if (isHTML(details.url, contentType) ||
+        isJS(details.url, contentType) ||
+        isCSS(details.url, contentType)) {
+        updateResult("alltext", details.url);
+    }
+}
+
+function launchWebsite(siteStr) {
+    return browser.tabs.create({ url: siteStr })
+    .then(function (tab) {
+        currentTab = tab.id;
+        console.log(`Launching: ${siteStr}`);
+    })
+    .delay(delayAmount)
+    .then(function(){
+        var currentTabCopy = currentTab;
+        currentTab = -2;
+        allResults[siteStr] = currentResult;
+        currentResult = JSON.parse(JSON.stringify(emptyResult));
+        console.log("Intermediate result: " + JSON.stringify(allResults));
+        return browser.tabs.remove(currentTabCopy);
+    });
+}
+
+function openPages() {
+    if (inProgress) { return; }
+    inProgress = true;
+
+    var p  = Promise.resolve(true);
+
+    sites.forEach(function(item){
+        p = p.then(function(){ return launchWebsite(item); });
+    });
+
+    p.then(function(){
+        console.log("Results: " + JSON.stringify(allResults));
+        allResults = {};
+        inProgress = false;
+    });
+}
+
+browser.webRequest.onHeadersReceived.addListener(
+    logURL,
+    {urls: ["<all_urls>"]},
+    ["blocking", "responseHeaders"]
+  );
+
+browser.browserAction.onClicked.addListener(openPages);
+
+//////////////////////////////////////////////////////////////////////////////////
+//General Helpers
+
+function delay(t, v) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve.bind(null, v), t)
+    });
+ }
+
+Promise.prototype.delay = function(t) {
+     return this.then(function(v) {
+         return delay(t, v);
+     });
+ }
+
+function last(arr){
+    if (!arr) { return arr; }
+    if (arr.length == 0) { return null; }
+    return arr[arr.length - 1];
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+//Content Helpers
+function ext(url) {
+    return (url = url.substr(1 + url.lastIndexOf("/")).split('?')[0]).split('#')[0].substr(url.lastIndexOf(".")).toLowerCase()
+}
+
+function checkUrlOrContentType(url, contentType, extensions, targetContentType){
+    if(!contentType) {
+        return extensions.includes(ext(url));
+    }
+
+    return contentType.value.toLowerCase().startsWith(targetContentType);
+}
+
+function isHTML(url, contentType){
+    return checkUrlOrContentType(url, contentType, ["", ".html"], "text/html");
+}
+
+function isJS(url, contentType){
+    return checkUrlOrContentType(url, contentType, [".js"], "application/javascript");
+}
+
+function isCSS(url, contentType){
+    return checkUrlOrContentType(url, contentType, [".css"], "text/css");
+}
+
+function isJPEG(url, contentType){
+    return checkUrlOrContentType(url, contentType, [".jpg", ".jpeg"], "image/jpeg");
+}
+
+function isPNG(url, contentType){
+    return checkUrlOrContentType(url, contentType, [".png"], "image/png");
+}
+
+function isGZipped(contentEncoding){
+    if (!contentEncoding) { return false; }
+    if (contentEncoding.value.toLowerCase() == "gzip") { return "true"; }
+    return false;
+}
+
+function isCompressed(contentEncoding){
+    if (!contentEncoding) { return false; }
+    if (contentEncoding.value) { return "true"; }
+    return false;
+}
+
+function getOrigin(urlString) {
+    const url = new URL(urlString);
+    return url.origin;
+}
