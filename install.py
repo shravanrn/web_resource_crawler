54a2cba178544bed151f336d6fa5ea744c215e41
Craig Disselkoen <craigdissel@gmail.com>
2019-08-22 17:23:01 -0700

    install script now works on macOS as well

--- install.py
+++ install.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 import os
+import platform
 
 currDir = os.path.dirname(os.path.realpath(__file__))
 nativeManifestPath = os.path.join(currDir, "webresourcecrawler_native.json")
 nativePythonPath = os.path.join(currDir, "webresourcecrawler_native.py")
-targetDir = os.path.expanduser("~/.mozilla/native-messaging-hosts")
+system = platform.system()
+if system == "Linux":
+    targetDir = os.path.expanduser("~/.mozilla/native-messaging-hosts")
+elif system == "Darwin":
+    targetDir = os.path.expanduser("~/Library/Application Support/Mozilla/NativeMessagingHosts")
+else:
+    raise ValueError("This script only works on Linux and macOS; got system == " + system)
 targetPath = os.path.join(targetDir, "webresourcecrawler_native.json")
 
 with open(nativeManifestPath) as f:
